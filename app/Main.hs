818fda838a1cdcf70edcb96797859eadcc0e52ef
0xd34df00d <0xd34df00d@gmail.com>
2024-09-01 13:16:54 -0500

    HashMap for lulz

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,22 @@
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE Strict #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE QuantifiedConstraints #-}
 
 module Main(main) where
 
 import Control.Applicative
 import Control.Monad.ST
-import Data.Array.ST qualified as A
-import Data.Array.Base qualified as A
 import Data.ByteString qualified as BS
 import Data.Functor
-import Data.EnumMap.Strict qualified as EM
+import Data.HashMap.Strict qualified as HM
 import Data.Vector.Unboxed.Mutable qualified as VM
 import Data.Word
 import System.Environment
 import System.IO.MMap
+import Data.Hashable
 
 -- * Definitions
 
-type StateId q = (Integral q, Enum q)
+type StateId q = (Integral q, Enum q, Hashable q)
 
 data Trans q
   = TEps q
@@ -27,7 +24,7 @@ data Trans q
   | TCh Word8 q
   deriving (Eq, Show)
 
-type TransMap q = EM.EnumMap q (Trans q)
+type TransMap q = HM.HashMap q (Trans q)
 
 data NFA q = NFA
   { transitions :: TransMap q
@@ -52,31 +49,25 @@ instance Alternative MatchResult where
 -- * Matching
 
 getTrans :: StateId q => q -> TransMap q -> Trans q
-getTrans q m = case q `EM.lookup` m of
+getTrans q m = case q `HM.lookup` m of
                  Just t -> t
                  Nothing -> error "invariant failure"
 
-match :: (StateId q, (forall s. A.MArray (A.STUArray s) q (ST s))) => NFA q -> BS.ByteString -> MatchResult Int
+match :: (VM.Unbox q, StateId q) => NFA q -> BS.ByteString -> MatchResult Int
 match NFA{..} bs = runST $ do
-  qstack <- mkArr 24_000_000
-  istack <- mkArr 24_000_000
+  stack <- VM.unsafeNew 24_000_000
   let go s q i
         | q == finState = pure $ SuccessAt i
         | otherwise = case q `getTrans` transitions of
               TEps q' -> go s q' i
-              TBranch q1 q2 -> do A.unsafeWrite qstack s q2
-                                  A.unsafeWrite istack s i
+              TBranch q1 q2 -> do VM.unsafeWrite stack s (q2, i)
                                   go (s + 1) q1 i
               TCh ch q'
                 | bs `BS.indexMaybe` i == Just ch -> go s q' (i + 1)
                 | s == 0 -> pure Failure
-                | otherwise -> do q'' <- A.unsafeRead qstack (s - 1)
-                                  i'' <- A.unsafeRead istack (s - 1)
+                | otherwise -> do (q'', i'') <- VM.unsafeRead stack (s - 1)
                                   go (s - 1) q'' i''
   go 0 initState 0
-  where
-  mkArr :: A.MArray (A.STUArray s) e (ST s) => Int -> ST s (A.STUArray s Int e)
-  mkArr len = A.unsafeNewArray_ (0, len)
 
 -- * Benchmarking
 
@@ -85,7 +76,7 @@ nfa = NFA{..}
   where
   initState = 0
   finState = 13
-  transitions = EM.fromList
+  transitions = HM.fromList
     [ (0, TBranch 2 1)
     , (1, TEps 12)
     , (2, TBranch 4 8)
