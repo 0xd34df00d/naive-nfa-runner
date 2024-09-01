eec0bf806a3ad2c9cf0c12905aed049dbd8e3f06
0xd34df00d <0xd34df00d@gmail.com>
2024-09-01 12:33:31 -0500

    Direct arrays

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE Strict #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 
 module Main(main) where
 
 import Control.Applicative
 import Control.Monad.ST
 import Data.Array.ST qualified as A
+import Data.Array.Base qualified as A
 import Data.ByteString qualified as BS
 import Data.Functor
 import Data.EnumMap.Strict qualified as EM
@@ -53,21 +56,27 @@ getTrans q m = case q `EM.lookup` m of
                  Just t -> t
                  Nothing -> error "invariant failure"
 
-match :: (VM.Unbox q, StateId q) => NFA q -> BS.ByteString -> MatchResult Int
+match :: (StateId q, (forall s. A.MArray (A.STUArray s) q (ST s))) => NFA q -> BS.ByteString -> MatchResult Int
 match NFA{..} bs = runST $ do
-  stack <- VM.unsafeNew 24_000_000
+  qstack <- mkArr 24_000_000
+  istack <- mkArr 24_000_000
   let go s q i
         | q == finState = pure $ SuccessAt i
         | otherwise = case q `getTrans` transitions of
               TEps q' -> go s q' i
-              TBranch q1 q2 -> do VM.unsafeWrite stack s (q2, i)
+              TBranch q1 q2 -> do A.unsafeWrite qstack s q2
+                                  A.unsafeWrite istack s i
                                   go (s + 1) q1 i
               TCh ch q'
                 | bs `BS.indexMaybe` i == Just ch -> go s q' (i + 1)
                 | s == 0 -> pure Failure
-                | otherwise -> do (q'', i'') <- VM.unsafeRead stack (s - 1)
+                | otherwise -> do q'' <- A.unsafeRead qstack (s - 1)
+                                  i'' <- A.unsafeRead istack (s - 1)
                                   go (s - 1) q'' i''
   go 0 initState 0
+  where
+  mkArr :: A.MArray (A.STUArray s) e (ST s) => Int -> ST s (A.STUArray s Int e)
+  mkArr len = A.unsafeNewArray_ (0, len)
 
 -- * Benchmarking
 
